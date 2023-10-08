cd38025d7fe890e73a4306a83d82b30b2c75e78a
Team-Finsight <135005281+Team-Finsight@users.noreply.github.com>
2023-10-08 17:57:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def main():
     load_dotenv()
     # Initialize session state
     initialize_session_state()
-    st.title("Multi-Docs ChatBot using llama2 :books:")
+    st.title("FIN-DOC-AI :🏬📉:")
     # Initialize Streamlit
     st.sidebar.title("Document Processing")
     uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
