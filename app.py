c5bf2c1fef4361563045f93e571ba69c7dfb7e15
Team-Finsight <135005281+Team-Finsight@users.noreply.github.com>
2023-10-10 19:44:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,8 +66,20 @@ def create_conversational_chain(vector_store):
                         #streaming=True, 
                         #callbacks=[StreamingStdOutCallbackHandler()],
                         #model_type="llama", config={'max_new_tokens': 500, 'temperature': 0.01})
-    llm = Replicate(model="a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",input={"temperature": 0.75,"max_length": 3500,"top_p": 1},)
+import torch
 
+llm = HuggingFaceLLM(
+    context_window=4096,
+    max_new_tokens=2048,
+    generate_kwargs={"temperature": 0.0, "do_sample": False},
+    system_prompt=system_prompt,
+    query_wrapper_prompt=query_wrapper_prompt,
+    tokenizer_name="meta-llama/Llama-2-13b-chat-hf",
+    model_name="meta-llama/Llama-2-13b-chat-hf",
+    device_map="auto",
+    # uncomment this if using CUDA to reduce memory usage
+    model_kwargs={"torch_dtype": torch.float16 , "load_in_8bit":True}
+)
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
     chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type='stuff',
